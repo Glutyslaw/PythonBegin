f5fbf147f035714f1685eb402a6f5f797d4b5602
Glutyslaw <58274660+Glutyslaw@users.noreply.github.com>
2019-11-28 00:45:21 +0100

    NextSteps
    With smal Steps

---
+++ InputField.py
@@ -0,0 +1,12 @@
+import sys
+
+e1 = int(input('Jetzt Zahl eingeben: '))
+e2 = int(input('Zweite Zahl: '))
+e3 = int(input('Und die drite...:'))
+
+
+
+summe = e1 + e2 + e3
+
+print(summe)
+
---
+++ WhileSchleiffe.py
@@ -0,0 +1,15 @@
+i = 0
+while i < 6:
+    print(i)
+    i = i + 1
+print('nach der while schleiffe folgt 6')
+
+liste =[7,5,4,3,2,5,6,675,4,34,2,43,234,34,23]
+
+summe = 0
+
+for element in liste:
+    print(element)
+    summe = summe + element
+
+print(summe)
